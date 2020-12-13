3e656ac0cef4c89c1f9147decc7fa76d86cc0c5f
MostwantedRBX <mostwantedrbxsteam@gmail.com>
2020-12-12 23:42:43 -0600

    Testing Atom Commits

--- WeeklyKeystoneTracker.lua
+++ WeeklyKeystoneTracker.lua
@@ -21,14 +21,26 @@ OpenAllBags()
   end
 end
 
+
+local function eventHandler(self,event,arg1)
+  if event == "PLAYER_ENTERING_WORLD" then
+    OpenAllBags()
+    local bag, slot = searchForKey()
+    print(string.find(GetContainerItemLink(bag,slot),"Spires of Ascension"))
+  elseif event == "ADDON_LOADED" then
+    if not WEEKLYKEYSTONETRACKER_KEY then
+      WEEKLYKEYSTONETRACKER_KEY = {}
+      WEEKLYKEYSTONETRACKER_KEY.taco = "Taco"
+      print("setting variable")
+    elseif WEEKLYKEYSTONETRACKER_KEY then
+      print(WEEKLYKEYSTONETRACKER_KEY.taco)
+    end
+  end
+end
+
+
 local frame = CreateFrame("FRAME","WeeklyKeystoneTracker")
 frame:RegisterEvent("PLAYER_ENTERING_WORLD")
+frame:RegisterEvent("ADDON_LOADED")
 print("frame registered")
-local function eventHandler(self,event)
-  print("eventHandler")
-  OpenAllBags()
-  local bag, slot = searchForKey()
-  print(string.find(GetContainerItemLink(bag,slot),"Spires of Ascension"))
-
-end
 frame:SetScript("OnEvent", eventHandler)
