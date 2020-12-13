ef5291c85858b48c73023dc6adde4a24fff5d9d7
Max Herold <maxoheyn@gmail.com>
2020-12-13 06:19:30 -0500

    formatting and README (#2)
    * minor additions, formatting overhaul
    
    * suggestion
    
    * README update

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # WeeklyKeystoneTracker
 
-This is Jay's addon!
+WeeklyKeystoneTracker makes it easy to see what keys you've done this week across all of your characters.
+
+
+Development Goals:
+- ✅ Determine if a player has a Mythic+ Keystone.
+- Identify the type of Mythic+ Keystone.
+- Store the player's current Mythic+ Keystone information.
+- Update the player's stored information upon getting a new Mythic+ Keystone.
+- Either:
+    - Keep a log of every week's Mythic+ runs.
+    - or clear log and show only current week's Mythic+ runs.
+- User interface, probably?
+
+Stretch Goals:
+- Store loot recieved in Mythic+ Dungeons.
--- WeeklyKeystoneTracker.lua
+++ WeeklyKeystoneTracker.lua
@@ -1,69 +1,80 @@
-
---
 print("WeeklyKeystoneTracker Starting")
 
- local function searchForKey()
-  print("searchForKey() running")
-  for i=0,4 do
-     local slots = GetContainerNumSlots(i)
-    if slots then
-      for s=1,slots do
-        if GetContainerItemID(i,s) == 180653 then
-          --debug
-          print('Bag: '..i..'Slots in bag: '..slots ..'Slot Key is in: '.. s)
-          print("key found")
-          --end of debug
-          link = GetContainerItemLink(i,s)
-          return link
-        end
-        s=s+1
-      end
-    end
-    i=i+1
-  end
+-- Returns an ItemLink for a mythic keystone if a player has one in their inventory.
+-- Returns nil otherwise.
+-- Example: |cffa335ee|Hkeystone:180653:382:6:10:11:0:0|h[Keystone: Theater of Pain (6)]|h|r
+local function searchForKey()
+	print("searchForKey() running")
+	for bag = 0, 4, 1 do
+		local slotsInBag = GetContainerNumSlots(bag)
+		if slotsInBag then
+			for slot = 1, slotsInBag, 1 do
+				if GetContainerItemID(bag, slot) == 180653 then
+					-- debug
+					print('Bag: ' .. bag .. '\nSlots in bag: ' ..slotsInBag .. '\nSlot Key is in: ' .. slot)
+					-- end of debug
+					linkToKey = GetContainerItemLink(bag, slot)
+					return linkToKey
+				end
+			end
+		end
+	end
 end
---29
+
+
 local function extrapolateKey(link)
-  --[[working on this, next, %d is digits in a pattern
-  This below is the item code you get from GetContainerItemLink() that I'm trying to get the
-  instance id from
-  |cffa335ee|Hkeystone:180653:382:6:10:11:0:0|h[Keystone: Theater of Pain (6)]|h|r"
-  ]]
-  local key = searchForKey()
-  local instanceId = string.find(key,":%d%d%d:")
+	-- working on this, next, %d is digits in a pattern
+
+	-- Item Code: |cffa335ee|Hkeystone:180653:382:6:10:11:0:0|h[Keystone: Theater of Pain (6)]|h|r
+	-- |cffa335ee 						= color information
+	-- |H								= hyperlink information start
+	-- keystone:180653:382:6:10:11:0:0 	= item information
+		-- :180653							= item id (mythic keystone)
+		-- :382								= dungeon/instance ID
+		-- :6								= key level
+		-- :10:11:0:0						= modifiers (assumed)
+	-- |h 								= End of link, text follows
+	-- [Keystone: Theater of Pain (6)] 	= actual display text
+	-- |h 								= end of hyperlink
+	-- |r								= restore color to normal
+
+	local key = searchForKey()
+	local instanceId = string.find(key, ":%d%d%d:")
+	-- can use substring since we know they are constants probably,
+	-- or colon delimeters rather than string.find
 end
 
-local function eventHandler(self,event,arg1)
-  if event == "PLAYER_ENTERING_WORLD" then
-    --just in case
-  elseif event == "ADDON_LOADED" then
-    --checks to see if you have saved variables
-    if not WEEKLYKEYSTONETRACKER then
-      WEEKLYKEYSTONETRACKER = {}
-      print("setting variable")
-    end
-    if WEEKLYKEYSTONETRACKER then
-      OpenAllBags()
-      print(string.find(searchForKey(),":%d%d%d:"))
-      --List of InstanceIDs
-      WEEKLYKEYSTONETRACKER.DungeonNames = {
-        [378] = "HOA",
-        [381] = "SOA",
-        [382] = "TOP",
-        [380] = "SD",
-        [376] = "WAKE",
-        [379] = "PF",
-        [377] = "DOS",
-        [375] = "MISTS"
-      }
-      --testing the table of names
-      print(WEEKLYKEYSTONETRACKER.DungeonNames[378])
-    end
-  end
+local function eventHandler(self, event, arg1)
+	if event == "PLAYER_ENTERING_WORLD" then 
+		-- just in case
+		elseif event == "ADDON_LOADED" then
+		-- checks to see if you have saved variables
+			if not WEEKLYKEYSTONETRACKER then
+				WEEKLYKEYSTONETRACKER = {}
+				print("setting variable")
+			end
+		
+		if WEEKLYKEYSTONETRACKER then
+			-- OpenAllBags() -- this probably needs to not be here because it messes with Bagnon
+			print(string.find(searchForKey(), ":%d%d%d:"))
+			-- List of InstanceIDs
+			WEEKLYKEYSTONETRACKER.DungeonNames = {
+				[378] = "HOA",  -- Halls of Atonement
+				[381] = "SOA",  -- Spires of Ascension
+				[382] = "TOP",  -- Theater of Pain
+				[380] = "SD",   -- Sanguine Depths
+				[376] = "WAKE", -- The Necrotic Wake
+				[379] = "PF",   -- Plaguefall
+				[377] = "DOS",  -- De Other Side
+				[375] = "MISTS" -- Mists of Tirna Scithe
+			}
+		end
+	end
 end
 
-
-local frame = CreateFrame("FRAME","WeeklyKeystoneTracker")
-frame:RegisterEvent("PLAYER_ENTERING_WORLD")
-frame:RegisterEvent("ADDON_LOADED")
-frame:SetScript("OnEvent", eventHandler)
+local eventFrame = CreateFrame("FRAME","WeeklyKeystoneTracker")
+eventFrame:RegisterEvent("PLAYER_ENTERING_WORLD")
+eventFrame:RegisterEvent("ADDON_LOADED")
+eventFrame:RegisterEvent("CHALLENGE_MODE_COMPLETED")
+eventFrame:RegisterEvent("BAG_UPDATE")
+eventFrame:SetScript("OnEvent", eventHandler)
