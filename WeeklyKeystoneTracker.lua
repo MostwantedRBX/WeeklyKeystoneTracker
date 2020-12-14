7b040f407d397f671559ac41e1a6393b4adc9aad
Max Herold <maxoheyn@gmail.com>
2020-12-13 22:41:22 -0500

    daily update (#3)
    * minor additions, formatting overhaul
    
    * suggestion
    
    * README update
    
    * savedvariables update

--- WeeklyKeystoneTracker.lua
+++ WeeklyKeystoneTracker.lua
@@ -1,25 +1,42 @@
-print("WeeklyKeystoneTracker Starting")
+--print("WeeklyKeystoneTracker Starting...")
 
 -- Returns an ItemLink for a mythic keystone if a player has one in their inventory.
 -- Returns nil otherwise.
--- Example: |cffa335ee|Hkeystone:180653:382:6:10:11:0:0|h[Keystone: Theater of Pain (6)]|h|r
+-- Example: [Keystone: Theater of Pain (6)]
 local function searchForKey()
-	print("searchForKey() running")
 	for bag = 0, 4, 1 do
 		local slotsInBag = GetContainerNumSlots(bag)
 		if slotsInBag then
 			for slot = 1, slotsInBag, 1 do
 				if GetContainerItemID(bag, slot) == 180653 then
-					-- debug
-					print('Bag: ' .. bag .. '\nSlots in bag: ' ..slotsInBag .. '\nSlot Key is in: ' .. slot)
-					-- end of debug
-					linkToKey = GetContainerItemLink(bag, slot)
+					--print('Bag: ' .. bag .. '\nSlots in bag: ' ..slotsInBag .. '\nSlot Key is in: ' .. slot)
+					local linkToKey = GetContainerItemLink(bag, slot)
 					return linkToKey
 				end
 			end
 		end
 	end
 end
+
+-- Separates the key with colons as the delimeter and stores it in a table
+-- Returns: a table with the individual parts of the key separated by the
+--          delimeter specified, otherwise defaults to a colon (:)
+local function splitKey (keyCode, delimeter)
+	print("splitting key")
+	if delimeter == nil then
+		delimeter = ":"
+	end
+
+	local t = {}
+	for str in string.gmatch(keyCode, "([^" .. delimeter .. "]+)") do
+		--print(str)
+		table.insert(t, str)
+	end
+	return t
+end
+
+-- Takes the keyLink provided and extracts the information from it
+-- Example: |cffa335ee|Hkeystone:180653:382:6:10:11:0:0|h[Keystone: Theater of Pain (6)]|h|r
 
 local function extrapolateKey(link)
 	-- working on this, next, %d is digits in a pattern
@@ -36,50 +53,82 @@ local function extrapolateKey(link)
 	-- [Keystone: Theater of Pain (6)] 	= actual display text
 	-- |h 								= end of hyperlink
 	-- |r								= restore color to normal
+local function extrapolateKey(linkToKey)
+	-- obtaining the key from player's bags
+	if linkToKey then
+		-- turn the link into values that we can read and split it
+		local keyCode = gsub(linkToKey, "\124", "\124\124");
+		local keyInfo = splitKey(keyCode, ":")
 
-	local key = searchForKey()
-	local instanceId = string.find(key, ":%d%d%d:")
-	-- can use substring since we know they are constants probably,
-	-- or colon delimeters rather than string.find
-end
+		-- assign the values to their variables
+		local instanceID = tonumber(keyInfo[3])
+		local keyLevel = tonumber(keyInfo[4])
+		local modifier1 = tonumber(keyInfo[5])
+		local modifier2 = tonumber(keyInfo[6])
+		local modifier3 = tonumber(keyInfo[7])
+		local modifier4 = tonumber(strsub(keyInfo[8], 0, 2))
 
-local function eventHandler(self, event, arg1)
-	if event == "PLAYER_ENTERING_WORLD" then
-		-- just in case
-	elseif event == "ADDON_LOADED" then
-		-- checks to see if you have saved variables
-			if not WEEKLYKEYSTONETRACKER then
-				WEEKLYKEYSTONETRACKER = {}
-				print("setting variable")
-			end
-
-		if WEEKLYKEYSTONETRACKER then
-			-- OpenAllBags() -- this probably needs to not be here because it messes with Bagnon
-			-- Does it search with the bag closed tho? If not we can open then close after the search
-			-- is done.
-			print(string.find(searchForKey(), ":%d%d%d:"))
-			-- List of InstanceIDs
-			WEEKLYKEYSTONETRACKER.DungeonNames = {
-				[378] = "HOA",  -- Halls of Atonement
-				[381] = "SOA",  -- Spires of Ascension
-				[382] = "TOP",  -- Theater of Pain
-				[380] = "SD",   -- Sanguine Depths
-				[376] = "WAKE", -- The Necrotic Wake
-				[379] = "PF",   -- Plaguefall
-				[377] = "DOS",  -- De Other Side
-				[375] = "MISTS" -- Mists of Tirna Scithe
-			}
+		-- in case modifier4 is nil we need to fix that
+		-- only in effect in keyLevel < 10
+		if modifier4 == nil then
+			modifier4 = tonumber(strsub(keyInfo[8], 0, 1))
 		end
-	elseif event == "CHALLENGE_MODE_COMPLETED" then
-		--stuff
-	elseif event == "BAG_UPDATE" then
-		--more stuff
 	end
 end
 
-local eventFrame = CreateFrame("FRAME","WeeklyKeystoneTracker")
-eventFrame:RegisterEvent("PLAYER_ENTERING_WORLD")
-eventFrame:RegisterEvent("ADDON_LOADED")
-eventFrame:RegisterEvent("CHALLENGE_MODE_COMPLETED")
-eventFrame:RegisterEvent("BAG_UPDATE")
-eventFrame:SetScript("OnEvent", eventHandler)
+-- handles any events that were registered to our EventFrame
+-- events handled:
+	-- PLAYER_LOGIN
+	-- ADDON_LOADED
+	-- CHALLENGE_MODE_COMPLETED
+	-- BAG_UPDATE
+local function eventHandler(self, event, arg1)
+	if event == "PLAYER_LOGIN" then
+		-- First-time setup
+		if not WEEKLYKEYSTONETRACKER_GLOBAL then
+			-- Start allocating stored variables upon first use.
+			-- Initializing some default values.
+			WEEKLYKEYSTONETRACKER_GLOBAL = {}
+			WEEKLYKEYSTONETRACKER_GLOBAL.Characters = {}
+			-- List of InstanceIDs
+			WEEKLYKEYSTONETRACKER_GLOBAL.DungeonNames = {
+				MISTS = 375, -- Mists of Tirna Scithe
+				WAKE = 376,  -- The Necrotic Wake
+				DOS = 377,   -- De Other Side
+				HOA = 378,   -- Halls of Atonement
+				PF = 379,    -- Plaguefall
+				SD = 380,    -- Sanguine Depths
+				SOA = 381,   -- Spires of Ascension
+				TOP = 382,   -- Theater of Pain
+			}
+		end
+
+		if not WEEKLYKEYSTONETRACKER_CHARACTER then
+			-- Per-character settings first time
+			WEEKLYKEYSTONETRACKER_CHARACTER = {}
+
+			WEEKLYKEYSTONETRACKER_CHARACTER.Character = UnitName("player")
+		end
+
+		-- End of first-time setup
+		
+		-- If this isn't the first login, we want to...
+		-- Store the player's current Keystone Information
+		-- 
+
+		-- End of PLAYER_LOGIN event
+	elseif event == "CHALLENGE_MODE_COMPLETED" then
+		print("Challenge mode completed!")
+		-- End of CHALLENGE_MODE_COMPLETED event
+	elseif event == "BAG_UPDATE" then
+		print("Bag updated.")
+
+	end
+end
+
+local EventFrame = CreateFrame("frame","EventFrame")
+EventFrame:RegisterEvent("PLAYER_LOGIN")
+EventFrame:RegisterEvent("ADDON_LOADED")
+EventFrame:RegisterEvent("CHALLENGE_MODE_COMPLETED")
+EventFrame:RegisterEvent("BAG_UPDATE")
+EventFrame:SetScript("OnEvent", eventHandler
